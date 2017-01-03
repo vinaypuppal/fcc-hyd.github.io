7e95ca8d869f7ce55dc25db5b6ec2f8340c378d4
Vinay Puppal <me@vinaypuppal.com>
2017-01-04 00:25:21 +0530

    members page half done

--- src/js/loadfont.js
+++ src/js/loadfont.js
@@ -4,7 +4,8 @@ import '../css/index.css'
 WebFont.load({
   google: {
     families: [
-      'Josefin Sans:400,300,100,600,700,700italic,600italic,400italic,300italic,100ital'
+      'Dosis:400,500,600,700',
+      'Josefin Sans:400,600,700'
     ]
   }
 })
